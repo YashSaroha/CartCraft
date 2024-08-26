2acdae60cef436536ff81942af01b50dd872dfbb
unknown <yashsaroha966@gmail.com>
2024-08-26 19:33:22 +0530

    changed hero image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,17 +4,18 @@ import { categories } from '../utils/constants'
 const Hero = () => {
     return (
         <div className='h-auto'>
-            <div className='bg-hero-pattern bg-cover bg-center h-[62vh] flex items-center'>
-                <div className='flex flex-col h-36 px-5'>
-                    <h1 className='text-xl font-bold text-gray-700'>#Big Fashion Sale</h1>
-                    <h1 className='font-bold text-3xl tracking-tighter'>Limited Time Offer!</h1>
-                    <h1 className='font-bold text-3xl tracking-tighter'>Upto <span className='italic text-pink-500'>50%</span> OFF!</h1>
-                    <h1 className='text-xl font-bold text-gray-700'>Redefine Your Everyday Style</h1>
+            <div className="bg-hero_2 bg-cover bg-center h-[67vh] relative">
+                <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent opacity-70"></div>
+                <div className='relative h-full w-full flex flex-col items-start justify-center px-5'>
+                    <h1 className='text-xl font-semibold text-gray-300 mb-2'>#Big Fashion Sale</h1>
+                    <h1 className='font-semibold text-3xl tracking-tighter text-white'>Limited Time Offer!</h1>
+                    <h1 className='font-semibold text-3xl tracking-tighter text-white'>Upto <span className='italic text-pink-500'>50%</span> OFF!</h1>
+                    <h1 className='text-xl font-semibold text-gray-300 mt-6'>Redefine Your Everyday Style</h1>
                 </div>
             </div>
-            <div className='w-full h-[33vh] py-6 px-2 md:h-[175px] flex flex-wrap items-center justify-center md:py-2 md:px-6 overflow-x-auto md:overflow-x-hidden'>
+            <div className='w-full h-[30vh] py-4 px-2 md:h-[145px] flex flex-wrap items-center justify-center md:py-2 md:px-6 overflow-x-auto md:overflow-x-hidden'>
                 {categories.map((category) => (
-                    <div key={category.name} className='w-[14%] h-[45%] mx-4 md:w-auto md:h-auto md:mx-10 flex flex-col items-center justify-center rounded-full cursor-pointer'>
+                    <div key={category.name} className='w-[14%] h-[30%] mx-4 md:w-auto md:h-auto md:mx-10 flex flex-col items-center justify-center rounded-full cursor-pointer'>
                         <span className='p-3 md:w-16 md:h-16 md:p-4 bg-zinc-200 hover:-translate-y-2 duration-200 rounded-full'>
                             <img src={category.img} className='w-full h-full' />
                         </span>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,9 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/src/assets/hero_img.jpg')",
+        'hero_1': "url('/src/assets/hero_1.webp')",
+        'hero_2': "url('/src/assets/hero_2.webp')",
+        'hero_3': "url('/src/assets/hero_img.jpg')",
       }
     },
   },
